0977dfef161db3e06d651ceaec087b1ccdff0506
Zane Shelby <zane@users.noreply.github.com>
2023-03-20 19:45:51 -0400

    feat: Add implementation of importance resampling

---
+++ src/gen/inference/importance.clj
@@ -0,0 +1,22 @@
+(ns gen.inference.importance
+  (:require [clojure.math :as math]
+            [fastmath.core :as fastmath]
+            [gen.distribution.fastmath :as dist]
+            [gen.dynamic]
+            [gen.generative-function :as gf]))
+
+(defn resampling [gf args observations n-samples]
+  ;; https://github.com/probcomp/Gen.jl/blob/master/src/inference/importance.jl#L77...L95
+  (let [result (gf/generate gf args observations)
+        model-trace (volatile! (:trace result))
+        log-total-weight (volatile! (:weight result))]
+    (dotimes [_ (dec n-samples)]
+      (let [candidate (gf/generate gf args observations)
+            candidate-model-trace (:trace candidate)
+            log-weight (:weight candidate)]
+        (vswap! log-total-weight #(fastmath/logsumexp [log-weight %]))
+        (when (dist/bernoulli (math/exp (- log-weight @log-total-weight)))
+          (vreset! model-trace candidate-model-trace))))
+    (let [log-ml-estimate (- @log-total-weight (math/log n-samples))]
+      {:trace @model-trace
+       :weight log-ml-estimate})))
