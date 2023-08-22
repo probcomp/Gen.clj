1c182eac0abffcd6ff6f5e1626b4f9a7f996ee39
Sam Ritchie <sam@mentat.org>
2023-08-21 20:21:02 -0600

    build: remove missing emacs variable

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,4 @@
 ((nil
   . ((cider-default-cljs-repl . node)
      (cider-preferred-build-tool . clojure-cli)
-     (cider-clojure-cli-aliases . ":test:nextjournal/clerk")
-
-     ;; Custom indentation:
-     (eval . (put-clojure-indent 'gen 1)))))
+     (cider-clojure-cli-aliases . ":test:nextjournal/clerk"))))
